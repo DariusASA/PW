13ddade145fa0552463d24d28074159577b237ef
Asargiu Daris <dariusasargiu@gmail.com>
2026-02-25 10:19:41 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact-form.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    console.log("Nume:", name);
+    console.log("Email:", email);
+    console.log("Mesaj:", message);
+ console.warn("Goodbye World!");
+}
